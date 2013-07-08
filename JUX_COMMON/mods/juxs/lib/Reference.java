1ec39ea9f8d8019eaa5500818946026532061a67
TheBassist95 <jshowe95@gmail.com>
2013-07-07 23:38:36 -0500

    Update Reference.java

--- JUX_COMMON/mods/juxs/lib/Reference.java
+++ JUX_COMMON/mods/juxs/lib/Reference.java
@@ -3,7 +3,7 @@ package mods.juxs.lib;
 public class Reference {
     public static final String MOD_ID="juxs";
     public static final String MOD_NAME="Juxs!";
-    public static final String VERSION="0.1.1";
+    public static final String VERSION="0.2.0";
     public static final String CHANNEL=MOD_ID;
     public static String MOD_DIR;
 }
