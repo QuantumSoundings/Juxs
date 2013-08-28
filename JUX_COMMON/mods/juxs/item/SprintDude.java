aef39e5fa0f9dd3f4ccd9579a38427ab98fbc687
Jordan <Jordan@10.146.16.128>
2013-08-28 15:40:23 -0500

    Stuff changed I guess. Its been a while since I've worked on this and im not sure of the current status of the project.

--- JUX_COMMON/mods/juxs/item/SprintDude.java
+++ JUX_COMMON/mods/juxs/item/SprintDude.java
@@ -1,6 +1,8 @@
 package mods.juxs.item;
 
 
+import java.util.List;
+
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -26,7 +28,6 @@ public class SprintDude extends Item{
 	}
 	@SideOnly(Side.CLIENT)
     public void registerIcons(IconRegister iconRegister) {
-
         itemIcon = iconRegister.registerIcon("juxs:sprintdude");
     }
 	@Override
@@ -65,13 +66,28 @@ public class SprintDude extends Item{
 			player.openGui(Juxs.instance, 1, world,(int) player.posX,(int) player.posY,(int) player.posZ);
         return par1ItemStack;
     }
-
+	@SideOnly(Side.CLIENT)
+	@Override
+	public boolean onDroppedByPlayer(ItemStack item, EntityPlayer player)
+    {
+		
+		if(!JuxsSoundManager.isPaused)
+			JuxsSoundManager.pauseOrPlayPersonal();
+        return true;
+    }
 	@SideOnly(Side.CLIENT)
 	public void playNext(Entity player){
 		JuxsSoundManager.stopPersonal();
 		curr=(curr+1)%RadioInit.getStation(currPlayList).songs.size();
 		currSong=RadioInit.getStation(currPlayList).songs.get(curr);
 		JuxsSoundManager.playPersonal((int)player.posX, (int)player.posY, (int)player.posZ, currSong,currPlayList);
+	}
+	@Override
+	public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List list, boolean par4){
+		list.add("Currently Playing: "+currSong.substring(0,currSong.length()-4));
+		list.add("Current PlayList: "+currPlayList);
+		list.add(paused?"Paused":"Playing");
+		
 	}
 
 }
